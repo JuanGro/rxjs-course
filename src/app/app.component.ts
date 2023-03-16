577bca50d1150a009b35aded5752cc5bd1d26118
Juan Guerrero <juan_guerrero@epam.com>
2023-03-16 17:30:08 -0600

    Create an observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs-course';
+  observable$: any;
+
+  ngOnInit() {
+    this.observable$ = new Observable((observer) => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.complete();
+    });
+    this.observable$.subscribe(
+      (value: any) => console.log(value),
+      (err :any) => {},
+      () => console.log('this is the end')
+    );
+  }
+
+  ngOnDestroy() {
+    this.observable$.unsubscribe();
+  }
 }
